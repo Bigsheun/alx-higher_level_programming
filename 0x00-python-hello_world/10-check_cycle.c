1cd930198e8293aa715142079ab67ed4390d860a
Bigsheun <bigsheun1812@gmail.com>
2023-01-03 13:12:13 +0100

    $1

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,102 +1,27 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
 /**
-* print_listint - prints all elements of a listint_t list
-* @h: pointer to head of list
-* Return: number of nodes
-*/
-size_t print_listint(const listint_t *h)
-{
-	const listint_t *current;
-	unsigned int n; /* number of nodes */
-
-	current = h;
-	n = 0;
-	while (current != NULL)
-	{
-		printf("%i\n", current->n);
-		current = current->next;
-		n++;
-	}
-
-	return (n);
-}
-
-/**
-* add_nodeint - adds a new node at the beginning of a listint_t list
-* @head: pointer to a pointer of the start of the list
-* @n: integer to be included in node
-* Return: address of the new element or NULL if it fails
-*/
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-	listint_t *new;
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-
-	new->n = n;
-	new->next = *head;
-	*head = new;
-
-	return (new);
-}
-
-/**
-* free_listint - frees a listint_t list
-* @head: pointer to list to be freed
-* Return: void
-*/
-void free_listint(listint_t *head)
-{
-	listint_t *current;
-
-	while (head != NULL)
-	{
-		current = head;
-		head = head->next;
-		free(current);
-	}
-}
-
-/**
-* check_cycle - checks  for cycle in a singly LL
-* @list: said list
-* Return: true/false
+* check_cycle - function checks if a singly linked list has a cycle in it.
+* @list: pointer to the beginning of the node
+* Return: 0 if no cycle, 1 if there is a cycle
 */
 int check_cycle(listint_t *list)
 {
-	int N = 1, count, flag = 1;
-	listint_t *node1, *node2, *head;
+	listint_t *node1, *node2;
 
-	head = list;
-	if (head == NULL)
+	if (list == NULL || list->next == NULL)
 		return (0);
+	node1 = list;
+	node2 = node1->next;
 
-	node1 = head->next;
-	if (node1 == NULL)
-		return (0);
-
-	N = 1;
-	while (node1 != NULL && flag)
+	while (node1 != NULL &&
+		node2->next != NULL &&
+		node2->next->next != NULL)
 	{
-		node2 = head;
-		for (count = 0; count < N; ++count)
-		{
-			if (node1 == node2)
-			{
-				flag = 0;
-				count = N;
-				break;
-			}
-			node2 = node2->next;
-		}
-		N += 1;
+		if (node1 == node2)
+			return (1);
 		node1 = node1->next;
+		node2 = node2->next->next;
 	}
-
-	return (!flag);
+	return (0);
 }
